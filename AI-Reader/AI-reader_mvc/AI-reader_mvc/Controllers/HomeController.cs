b7faf3908fa2f7a3a2abef73417492c5a01acb6f
Alepy369 <duartea369@gmail.com>
2023-06-14 14:01:04 +0100

    Digital TechJoint code

---
+++ AI-Reader/AI-reader_mvc/AI-reader_mvc/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using AI_reader_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AI_reader_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AI-Reader/AI-reader_mvc/AI-reader_mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AI_reader_mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AI_reader_mvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+    }
+}
