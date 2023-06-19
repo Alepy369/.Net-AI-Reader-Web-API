f6aad37cb49ba67ad3ae5e78df033f8d5eb4b778
Alepy369 <duartea369@gmail.com>
2023-06-19 12:10:01 +0100

    Uptade code

--- AI-Reader/AI-reader_mvc/AI-reader_mvc/Models/User.cs
+++ AI-Reader/AI-reader_mvc/AI-reader_mvc/Models/User.cs
@@ -1,24 +1,17 @@
-﻿namespace AI_reader_mvc.Models
-{
-    public class User
-    {
-#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
-        public User()
-#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
-        {
-        }
-            
-        public int Id { get; set }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        //public string Phone { get; set; }
-        //public string Address { get; set; }
-        //public string City { get; set; }
-        //public string Region { get; set; }
-        //public string PostalCode { get; set; }
-        //public string Country { get; set; }
-    }
+﻿namespace AI_reader_mvc.Models;
+
+public partial class User
+{ 
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string LastName { get; set; } = null!;
+    public string? Email { get; set; } = null!;  
+    public string? Password { get; set; }
+    public string? PhoneNumber { get; set; }
+    //public string Phone { get; set; }
+    //public string Address { get; set; }
+    //public string City { get; set; }
+    //public string Region { get; set; }
+    //public string PostalCode { get; set; }
+    //public string Country { get; set; }
 }
--- AI-Reader/AI-reader_mvc/AI-reader_mvc/Program.cs
+++ AI-Reader/AI-reader_mvc/AI-reader_mvc/Program.cs
@@ -1,8 +1,5 @@
 using AI_reader_mvc.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+options.UseSqlServer(builder.Configuration.GetConnectionString("AiReader")));
 
 var app = builder.Build();
 
